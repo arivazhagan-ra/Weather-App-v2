a8534c33a514a90432e2ad141ec203ca9ef22a9f
arivazhagan-ra <arivazhangan14@example.com>
2025-11-27 08:32:38 -0800

    updated version

--- script.js
+++ script.js
@@ -7,56 +7,51 @@ let description = document.getElementById("description");
 let clouds = document.getElementById("clouds");
 let humidity = document.getElementById("humidity");
 let pressure = document.getElementById("pressure");
+let blurLayer = document.getElementById("blurLayer");
+let loader = document.getElementById("loader");
 
-
+let id="3758104ac86bfb4d17dfa1671332c3c1";
 let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=`+id;
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const userInput = search.value.trim();
     // Basic validation: check for empty or numeric input
-    if (userInput === "") {
-        alert("Please enter a city name");
-         cards.classList.add("hidden");
-    setTimeout(() => {
-        cards.classList.remove("hidden");
-    }, 1000);
-    return;
+    if (userInput === "" || !/^[a-zA-Z\s]+$/.test(userInput)) {
+       cards.classList.add("hidden");
+        setTimeout(() => {
+            cards.classList.remove("hidden");
+        }, 600);
+        return;
     }
-    // Reject purely numeric or invalid input
-    if (!/^[a-zA-Z\s]+$/.test(userInput)) {
-        alert("Please enter a valid city name");
-        description.textContent = "Please enter a valid city name";
-         cards.classList.add("hidden");
-    setTimeout(() => {
-        cards.classList.remove("hidden");
-    }, 1000);
-    return;
-    }
-    setWeather(userInput);
+    setWeather();
     console.log("Fetching weather data...");
-});
-search.addEventListener('keydown', (e) => {
+    });
+    search.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault();
         form.dispatchEvent(new Event('submit'));
     }
-});
+    });
 
-function setWeather() {
-      fetch(url + "&q=" + search.value)
-        .then(res => res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.cod===200){
-                city.querySelector("h4").textContent = data.name;
-                city.querySelector("img").src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
-                temperature.querySelector('img').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-                temperature.querySelector('span').textContent = data.main.temp;
-                description.textContent = data.weather[0].description;
-                clouds.textContent = data.clouds.all+ "%";
-                humidity.textContent = data.main.humidity+ "%";
-                pressure.textContent = data.main.pressure+ "hPa";
-            }
+        function setWeather() {
+        blurLayer.style.display = "block";
+        loader.style.display ="flex";
+        fetch(url + "&q=" + search.value)
+            .then(res => res.json())
+            .then(data=>{
+                console.log(data);
+                blurLayer.style.display = "none";
+                loader.style.display="none";
+                if(data.cod===200){
+                    city.querySelector("h4").textContent = data.name;
+                    city.querySelector("img").src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+                    temperature.querySelector('img').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                    temperature.querySelector('span').textContent = data.main.temp;
+                    description.textContent = data.weather[0].description;
+                    clouds.textContent = data.clouds.all+ "%";
+                    humidity.textContent = data.main.humidity+ "%";
+                    pressure.textContent = data.main.pressure+ "hPa";
+                }
             else{
                 city.querySelector("h4").textContent = "City Not Found";
                 temperature.querySelector('span').textContent = "";
